cd9c48bd15b43d5b397ebe4cb71407bfd89f12d4
hen-chen <henchen@seas.upenn.edu>
2021-06-28 16:11:45 -0400

    6/28

---
+++ README.md
@@ -0,0 +1,12 @@
+#discord_bot
+
+TODO
+
+##Priority
+Come up with idea
+Read through docs and watch some tutorials on JS and jsons. APIS
+
+Figure out how to host on the cloud
+
+##Extra fluff
+        
--- src/bot.js
+++ src/bot.js
@@ -1,15 +1,34 @@
 require('dotenv').config();
+const fetch = require("node-fetch");
 const Discord = require("discord.js");
 
 const client = new Discord.Client();
+let sadWords = ["sad", "mad", "bad"];
+let encouragements = ["Cheer Up, Henry!", "Hang in there, Henry.", "Hey there, Henry!"];
+
+function getQuote() {
+    return fetch("https://zenquotes.io/api/random")
+    .then(res => {
+        return res.json()
+    })
+    .then(data =>{
+        return data[0]["q"] + " -" + data[0]["a"]
+    })
+}
 
 client.on("ready", () => {
     console.log(`Logged in as ${client.user.tag}!`)
-})
+});
 
 client.on("message", msg => {
-    if (msg.content === "ping") {
-        msg.reply("pong");
+    if (msg.author.bot) return;
+
+    if (msg.content ===  "$inspire"){
+        getQuote().then(quote => msg.channel.send(quote))
+    }
+    if (sadWords.some(word => msg.content.includes(word))){
+        const encouragement = encouragements[Math.floor(Math.random() * encouragements.length)];
+        msg.reply(encouragement);
     }
 })
 
