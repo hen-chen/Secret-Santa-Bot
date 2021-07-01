a9c0e98e10e9d3130372fcb5ad80bbd2fb924e9b
hen-chen <henchen@seas.upenn.edu>
2021-07-01 16:44:06 -0400

    7/1

--- README.md
+++ README.md
@@ -3,19 +3,20 @@
 TODO
 
 ## Priority
-Secret Santa Bot
-Algorithm:
-    Host gives a time frame for people to enter <!host [time frame]>
-    People type !enter. <!enter>
-    After time frame. Randomly assign partners. Make sure no repeats.
-    DM each person their partner.
-
 Read through docs and watch some tutorials on JS and jsons. APIS
+
+Countdown before time to ask/get gifts ends
+
+Automatically ends after the host gives a time duration
+
 Figure out how to host on the cloud
 
 ## Fluff
 
-    If you have a question for your person, DM the bot and it will DM the person with the question (i.e.if person A has person B for secret santa, person A can DM the bot and the bot will ask something to person B)
-    Gift find command that fetches a random Amazon product that costs less than $15
-    Countdown before time to enter expires
-    Pictures
+Gift find command that fetches a random Amazon product that costs less than $15
+
+Wishlist?
+
+Countdown before time to enter expires
+    
+Pictures
--- src/bot.js
+++ src/bot.js
@@ -10,10 +10,25 @@ let hosting = false;
 let host = null;
 let pairings = {};
 
+//Help command
+client.on("message", msg => {
+    if (msg.author.bot) return;
+    if (msg.content === "!help") {
+        msg.reply("here is a list of everything I can do:" + 
+        "\n `!host` Allows participants to enter and leave. There can only be one host at a time" +
+        "\n `!cancel` Resets the participants list. Only available to the host" + 
+        "\n `!enter` Enters you into the Secret Santa" +
+        "\n `!leave` Lets you leave the Secret Santa" + 
+        "\n `!list` Lets you see everyone in the Secret Santa" +
+        "\n `!start` Starts the Secret Santa. Entering and leaving will no longer be available. Only available to the host." + 
+        "\n `!ask` DM me a question, and I will pass it on to your gift recipient");
+    }
+})
+
 //Hosting command
 client.on("message", msg => {
     if (msg.author.bot) return;
-
+    if(msg.channel.type === "dm") return;
     if (msg.content === "!host" && !hosting) {
         hosting = true;
         host = msg.author;
@@ -27,11 +42,9 @@ client.on("message", msg => {
 // Cancel hosting command
 client.on("message", msg => {
     if (msg.author.bot) return;
-
+    if(msg.channel.type === "dm") return;
     if (msg.content === "!cancel" && hosting && JSON.stringify(msg.author) === JSON.stringify(host)) {
-        hosting = false;
-        participants = [];
-        host = null;
+        resetSanta();
         msg.channel.send("Hosting has been cancelled.");
     } else if (msg.content === "!cancel" && !hosting) {
         msg.reply("There is no Secret Santa right now.");
@@ -78,9 +91,12 @@ client.on("message", msg => {
 client.on("message", msg => {
     if (msg.author.bot) return;
 
-    if (msg.content === "!list" && hosting) {
+    if (msg.content === "!list" && hosting && participants.length === 0) {
+        msg.channel.send("There are no participants as of now.");
+    } else if (msg.content === "!list" && hosting) {
         let message = "The current participants are:";
         participants.forEach(x => message += "\n" + x.username);
+        message += "\nThe host is " +  host.username;
         msg.channel.send(message);
     } else if (msg.content === "!list") {
         msg.channel.send("No host has started yet.");
@@ -90,22 +106,47 @@ client.on("message", msg => {
 // Start command
 client.on("message", msg => {
     if (msg.author.bot) return;
-
-    if (msg.content === "!start" && hosting) {
-        getSantas(participants);
+    if(msg.channel.type === "dm") return;
+    if (msg.content === "!start" && hosting && JSON.stringify(msg.author) !== JSON.stringify(host)) {
+        msg.reply(`you are not the host of this Secret Santa! Please ask ${host.username} to start.`);
+    }
+    else if (msg.content === "!start" && hosting && participants.length >= 3) {
+        participants = getSantas(participants);
+        console.log(participants);
         for (let i = 0; i < participants.length; i++) {
             if (i === participants.length - 1) {
                 pairings[participants[i]] = participants[0];
-                pairings[particpiants[i]].send(`${participants[0]} is your secret santa`);
+                participants[i].send(`${participants[0].username} is your secret santa`);
             } else {
                 pairings[participants[i]] = participants[i + 1];
-                pairings[particpiants[i]].send(`${participants[i + 1]} is your secret santa`);
+                participants[i].send(`${participants[i + 1].username} is your secret santa`);
             }
         }
+        //Ends hosting
+        participants = [];
         hosting = false;
         msg.channel.send("Secret Santa has begun!");
     } else if (msg.content === "!start" && !hosting) {
         msg.reply("you need to host before you can start. Try `!host`.");
+    } else if (msg.content === '!start' && participants.length < 3) {
+        msg.channel.send('Not enough participants. Need at least 3.');
+    }
+})
+
+// Relay a message
+client.on("message", msg => {
+    if (msg.author.bot) return;
+    if(msg.channel.type === "text") return;
+    if (msg.content.includes("!ask") && pairings.hasOwnProperty(msg.author)) {
+        //Parses question
+        let message = "Your secret Santa has asked: " + msg.content.substr(5);
+
+        //Find the secret santa
+        let secretSanta = pairings[msg.author];
+        secretSanta.send(message);
+        msg.author.send("Your question has been asked!");
+    } else if (msg.content.includes('!ask') && !pairings.hasOwnProperty(msg.author)) {
+        msg.author.send('Either you are not in the Secret Santa or the event has not yet begun. Ask your host for more details.');
     }
 })
 
@@ -125,6 +166,13 @@ const getSantas = people => {
     }
     return people;
 } 
+
+const resetSanta = () => {
+    participants = [];
+    hosting = false;
+    host = null;
+    pairings = {};
+}
 
 //Previous functions
 // function getQuote() {
