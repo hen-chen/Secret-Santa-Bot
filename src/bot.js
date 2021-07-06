b4860b850e28b20d80f18c5b23380760d17f190f
hen-chen <henchen@seas.upenn.edu>
2021-07-06 14:49:21 -0400

    fix date stuff

--- src/bot.js
+++ src/bot.js
@@ -13,27 +13,76 @@ let givingDict = {};
 let receivingDict = {};
 let dueDate = null;
 
-//Help command
+//Everything listener
 client.on("message", msg => {
     if (msg.author.bot) return;
+
+    //Help command
     if (msg.content === "!help") {
-        msg.reply("here is a list of everything I can do:" + 
+        msg.reply("here is a list of everything I can do:" +
         "\n `!host` Allows participants to enter and leave. There can only be one host at a time" +
-        "\n `!cancel` Resets the participants list. Only available to the host" + 
+        "\n `!cancel` Resets the participants list. Only available to the host" +
         "\n `!enter` Enters you into the Secret Santa" +
-        "\n `!leave` Lets you leave the Secret Santa" + 
+        "\n `!leave` Lets you leave the Secret Santa" +
         "\n `!list` Lets you see everyone in the Secret Santa" +
         "\n `!countdown` Lets you see how much time is left before Secret Santa ends" +
-        "\n `!start` Give me a date, and I'll assign everyoen their Secret Santa and set a countdown before the event ends. Entering and leaving will no longer be available. Only available to the host" + 
+        "\n `!start` Give me a date, and I'll assign everyoen their Secret Santa and set a countdown before the event ends. Entering and leaving will no longer be available. Only available to the host" +
         "\n `!ask` DM me a question, and I will pass it on to your gift recipient" +
         "\n `!reply` DM me something, and I will pass it on to your Secret Santa. You can answer a question they ask, or reuqest something of your own");
     }
+
+    // Enter command
+    if (msg.content === "!enter" && hosting && !participants.includes(msg.author)) {
+        participants.push(msg.author);
+        msg.channel.send(`Welcome ${msg.author.username}! You are in for this Secret Santa!`);
+    } else if (msg.content === "!enter" && hosting) {
+        msg.reply("you're already signed up.");
+    } else if (msg.content === "!enter" && !hosting && !start) {
+        msg.reply("Sorry! There isn't a Secret Santa right now.");
+    } else if (msg.content === "!enter" && !hosting && start) {
+        msg.reply(`Sorry, Secret Santa has alreay begun. Please ask ${host.username} about restarting if you want to withdraw.`);
+    }
+
+    // Leave command
+    if (msg.content === "!leave" && hosting && participants.includes(msg.author)) {
+        participants.splice(participants.indexOf(msg.author), 1);
+        msg.channel.send(`Goodbye ${msg.author.username}. See you next time!`);
+    } else if (msg.content === "!leave" && hosting) {
+        msg.reply("you're not signed up yet.");
+    } else if (msg.content === "!leave" && !hosting && !start) {
+        msg.reply("Sorry! There isn't a Secret Santa right now.");
+    } else if (msg.content === "!enter" && !hosting && start) {
+        msg.reply(`Sorry, Secret Santa has alreay begun. Please ask ${host.username} about restarting if you want to join.`);
+    }
+
+    // List all members of Secret Santa
+    if (msg.content === "!list" && hosting && participants.length === 0) {
+        msg.channel.send("There are no participants as of now.");
+    } else if (msg.content === "!list" && hosting) {
+        let message = "The current participants are:";
+        participants.forEach(x => message += "\n" + x.username);
+        message += "\nThe host is " + host.username;
+        msg.channel.send(message);
+    } else if (msg.content === "!list") {
+        msg.channel.send("No host has started yet.");
+    }
+
+    // Countdown
+    if (start && msg.content === "!countdown") {
+        msg.channel.send(milliToDays() + " days remaining!");
+    } else if (msg.content === "!countdown" && !start) {
+        msg.channel.send("Secret Santa has not started yet.");
+    }
 })
 
-//Hosting command
+
+//Channel listener
 client.on("message", msg => {
+
     if (msg.author.bot || msg.channel.type === "dm") return;
-    if (msg.content ==="!host" && !hosting && !start) {
+
+    //Hosting command
+    if (msg.content === "!host" && !hosting && !start) {
         hosting = true;
         host = msg.author;
         msg.channel.send(`Hosting has started! The host is ${host.username}.`);
@@ -41,12 +90,8 @@ client.on("message", msg => {
     } else if (msg.content === "!host" && hosting) {
         msg.reply("Hosting has already started. The host can use `!cancel` to cancel.");
     }
-})
 
-// Cancel hosting command
-client.on("message", msg => {
-    if (msg.author.bot) return;
-    if (msg.channel.type === "dm") return;
+    // Cancel hosting command
     if (msg.content === "!cancel" && hosting && JSON.stringify(msg.author) === JSON.stringify(host)) {
         resetSanta();
         msg.channel.send("Hosting has been cancelled.");
@@ -55,12 +100,8 @@ client.on("message", msg => {
     } else if (msg.content === '!cancel' && JSON.stringify(msg.author) !== JSON.stringify(host)) {
         msg.reply(`you are not the host of this Secret Santa! Please ask ${host.username} to cancel.`);
     }
-})
 
-// Reset command
-client.on("message", msg => {
-    if (msg.author.bot) return;
-    if (msg.channel.type === "dm") return;
+    // Reset command
     if (msg.content === "!reset" && start && JSON.stringify(msg.author) === JSON.stringify(host)) {
         resetSanta();
         msg.channel.send("Hosting has been cancelled.");
@@ -69,61 +110,8 @@ client.on("message", msg => {
     } else if (msg.content === '!reset' && JSON.stringify(msg.author) !== JSON.stringify(host)) {
         msg.reply(`you are not the host of this Secret Santa! Please ask ${host.username} to reset.`);
     }
-})
 
-// Enter command
-client.on("message", msg => {
-    if (msg.author.bot) return;
-
-    if (msg.content === "!enter" && hosting && !participants.includes(msg.author)) {
-        participants.push(msg.author);
-        //console.log(participants);
-        msg.channel.send(`Welcome ${msg.author.username}! You are in for this Secret Santa!`);
-    }
-    else if (msg.content === "!enter" && hosting) {
-        msg.reply("you're already signed up.");
-    }
-    else if (msg.content === "!enter" && !hosting) {
-        msg.reply("Sorry! There isn't a Secret Santa right now.");
-    }
-})
-
-// Leave command
-client.on("message", msg => {
-    if (msg.author.bot) return;
-
-    if (msg.content === "!leave" && hosting && participants.includes(msg.author)) {
-        participants.splice(participants.indexOf(msg.author), 1);
-        //console.log(participants);
-        msg.channel.send(`Goodbye ${msg.author.username}. See you next time!`);
-    }
-    else if (msg.content === "!leave" && hosting) {
-        msg.reply("you're not signed up yet.");
-    }
-    else if (msg.content === "!leave" && !hosting) {
-        msg.reply("Sorry! There isn't a Secret Santa right now.");
-    }
-})
-
-// List all members of Secret Santa
-client.on("message", msg => {
-    if (msg.author.bot) return;
-
-    if (msg.content === "!list" && hosting && participants.length === 0) {
-        msg.channel.send("There are no participants as of now.");
-    } else if (msg.content === "!list" && hosting) {
-        let message = "The current participants are:";
-        participants.forEach(x => message += "\n" + x.username);
-        message += "\nThe host is " +  host.username;
-        msg.channel.send(message);
-    } else if (msg.content === "!list") {
-        msg.channel.send("No host has started yet.");
-    }
-})
-
-// Start command
-client.on("message", msg => {
-    if (msg.author.bot || msg.channel.type === "dm") return;
+    // Start command
     if (msg.content === "!start" && hosting && JSON.stringify(msg.author) !== JSON.stringify(host)) {
         msg.reply(`you are not the host of this Secret Santa! Please ask ${host.username} to start.`);
     }
@@ -134,8 +122,20 @@ client.on("message", msg => {
             dueDate = null;
             return;
         }
-        let diff = milliToDays(); // testing to calc diff
-        console.log(diff);
+        let tempDate = new Date();
+        let currYear = dueDate.getFullYear();
+        if (currYear < tempDate.getFullYear()) {
+            msg.channel.send("You must specify the current year or a year in the future!");
+            dueDate = null;
+            return;
+        }
+        if (dueDate < tempDate) {
+            msg.channel.send("Specify a date in the future!");
+            dueDate = null;
+            return;
+        }
+        // let diff = milliToDays(); // testing to calc diff
+        // console.log(diff);
 
         participants = getSantas(participants);
         for (let i = 0; i < participants.length; i++) {
@@ -159,9 +159,11 @@ client.on("message", msg => {
     }
 })
 
-// Relay a message
+//DM listener
 client.on("message", msg => {
     if (msg.author.bot || msg.channel.type === "text") return;
+
+    // Relay a message
     if (msg.content.includes("!ask") && givingDict.hasOwnProperty(msg.author)) {
         //Parses question
         let message = "Your secret Santa has asked: " + msg.content.substr(5); // !ask is index 5
@@ -173,11 +175,8 @@ client.on("message", msg => {
     } else if (msg.content.includes('!ask') && !givingDict.hasOwnProperty(msg.author)) {
         msg.author.send('Either you are not in the Secret Santa or the event has not yet begun. Ask your host for more details.');
     }
-})
 
-// Respond to a question
-client.on("message", msg => {
-    if (msg.author.bot || msg.channel.type === "text") return;
+    // Respond to a question
     if (msg.content.includes("!reply") && givingDict.hasOwnProperty(msg.author)) {
         //Parses question
         let message = `${msg.author.username} says: ` + msg.content.substr(7);
@@ -193,22 +192,8 @@ client.on("message", msg => {
     } else if (msg.content.includes('!reply') && !givingDict.hasOwnProperty(msg.author)) {
         msg.author.send('Either you are not in the Secret Santa or the event has not yet begun. Ask your host for more details.');
     }
-})
 
-// Countdown
-client.on("message", msg => {
-    if (msg.author.bot) return;
-    if (start && msg.content === "!countdown") {
-        msg.channel.send(milliToDays() + " days remaining!");
-    } else if (msg.content === "!countdown" && !start) {
-        msg.channel.send("Secret Santa has not started yet");
-    }
-})
-
-// Find a gift
-client.on("message", msg => {
-    if (msg.author.bot) return;
-    if (msg.channel.type === "text") return;
+    // Find a gift
     if (msg.content.includes("!gift") && givingDict.hasOwnProperty(msg.author)) {
         let message = findGift();
         msg.author.send(message);
@@ -217,22 +202,24 @@ client.on("message", msg => {
     }
 })
 
+
+// Helper funcs
 const getSantas = people => {
     let currentIndex = people.length;
     let randomIndex = null;
 
     // While there remain elements to shuffle...
     while (0 !== currentIndex) {
-      // Pick a remaining element...
-      randomIndex = Math.floor(Math.random() * currentIndex);
-      currentIndex--;
-  
-      // And swap it with the current element.
-      [people[currentIndex], people[randomIndex]] = [
-        people[randomIndex], people[currentIndex]];
+        // Pick a remaining element...
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex--;
+
+        // And swap it with the current element.
+        [people[currentIndex], people[randomIndex]] = [
+            people[randomIndex], people[currentIndex]];
     }
     return people;
-} 
+}
 
 const resetSanta = () => {
     participants = [];
@@ -246,7 +233,7 @@ const resetSanta = () => {
 
 const milliToDays = () => {
     let currDate = new Date();
-    return Math.floor((dueDate - currDate) / 86400000);
+    return Math.ceil((dueDate - currDate) / 86400000);
 }
 
 const findGift = () => {
